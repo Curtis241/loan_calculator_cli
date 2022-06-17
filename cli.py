565ba59f1188f1472f25c70d6e18e90b2326cf2c
Curtis Peterson <copeterson07@gmail.com>
2022-06-16 20:49:37 -0600

    connected args to class

--- cli.py
+++ cli.py
@@ -23,11 +23,40 @@ class LoanCalculator:
         """
         return (self.__principal * self.__rate * self.__period_count) + self.__principal
 
+def calculate_simple_interest(args):
+    principal = args.principal
+    rate = args.rate
+    period_count = args.periods
+    calculator = LoanCalculator(principal, rate, period_count)
+    print(calculator.simple())
 
-parser = argparse.ArgumentParser(description='Calculates the cost of a loan.')
-parser.add_argument('--principal', action='store', type=int, required=True, help='Principal')
-parser.add_argument('--rate', action='store', type=float, required=True, help='Rate')
-parser.add_argument('--periods', action='store', type=int, required=True, help='Period count')
+def calculate_compound_interest(args):
+    principal = args.principal
+    rate = args.rate
+    period_count = args.periods
+    calculator = LoanCalculator(principal, rate, period_count)
+    print(calculator.compound())
 
-args = parser.parse_args()
-print(args)
+def setup():
+    parser = argparse.ArgumentParser(description='Calculates the cost of a loan')
+    sub_parsers = parser.add_subparsers()
+    simple_sp = sub_parsers.add_parser("simple")
+    simple_sp.add_argument('--principal', action='store', type=int, required=True, help='Principal')
+    simple_sp.add_argument('--rate', action='store', type=float, required=True, help='Rate')
+    simple_sp.add_argument('--periods', action='store', type=int, required=True, help='Period count')
+    simple_sp.set_defaults(func=calculate_simple_interest)
+
+    compound_sp = sub_parsers.add_parser("compound")
+    compound_sp.add_argument('--principal', action='store', type=int, required=True, help='Principal')
+    compound_sp.add_argument('--rate', action='store', type=float, required=True, help='Rate')
+    compound_sp.add_argument('--periods', action='store', type=int, required=True, help='Period count')
+    compound_sp.set_defaults(func=calculate_compound_interest)
+
+    return parser
+
+if __name__ == "__main__":
+    parser = setup()
+    args = parser.parse_args()
+    if "func" in args:
+        args.func(args)
+
